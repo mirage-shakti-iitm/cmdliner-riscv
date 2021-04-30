881a0e24a6484f6e5d0e0e1c5f7e6130b66c913e
SaiVK <saiganesha5.svkv@gmail.com>
2021-04-30 12:24:15 +0530

    Cleaned

--- compartment_allocations/strategy_1/cmdliner_suggest_cap_tee.ml
+++
@@ -1,7 +0,0 @@
-(*
-camlCmdliner_suggest__levenshtein_distance:11
-camlCmdliner_suggest__minimum:11
-camlCmdliner_suggest__value:11
-camlCmdliner_suggest__add:11
-camlCmdliner_suggest_:255
-*)
--- compartment_allocations/strategy_1/cmdliner_term_cap_tee.ml
+++
@@ -1,6 +0,0 @@
-(*
-camlCmdliner_term__const:11
-camlCmdliner_term__app:11
-camlCmdliner_term_:255
-camlCmdliner_term__fun:11
-*)
